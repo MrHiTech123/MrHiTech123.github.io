b4e31355dd0898569c795757d4ffa1581a7bd2c0
MrHiTech <mlebowitz97@gmail.com>
2024-11-01 12:38:57 -0400

    Added house of usher materials

---
+++ gsas/intro_game_design/lander/script.js
@@ -0,0 +1,18 @@
+
+var canvas;
+
+class asteroid() {
+	
+}
+
+function setup() {
+	canvas = CreateCanvas(600, 600);
+	document.getElementById("screen").parent(canvas);
+}
+
+function draw() {
+	
+}
+
+
+
